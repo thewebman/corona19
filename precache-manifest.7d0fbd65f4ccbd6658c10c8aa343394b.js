ee493f07d16b56100579d4e60ec2237dbd7b5b2f
thewebman <34786376+thewebman@users.noreply.github.com>
2020-06-03 02:09:03 -0400

    Add files via upload

---
+++ precache-manifest.7d0fbd65f4ccbd6658c10c8aa343394b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40f4eece54d157e1d517f68c4460d65",
+    "url": "/corona19/index.html"
+  },
+  {
+    "revision": "f982cbe155cb859e75f4",
+    "url": "/corona19/static/css/main.c5a4adb0.chunk.css"
+  },
+  {
+    "revision": "e5e72d9ca63f455d9f03",
+    "url": "/corona19/static/js/2.40868764.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/corona19/static/js/2.40868764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f982cbe155cb859e75f4",
+    "url": "/corona19/static/js/main.adf410f6.chunk.js"
+  },
+  {
+    "revision": "ada9ff0a7a4729aa52a8",
+    "url": "/corona19/static/js/runtime-main.29ef93d4.js"
+  }
+]);
